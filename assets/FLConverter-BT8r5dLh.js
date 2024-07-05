5fe5ee736bfcbafda4059121d2a070cb32fd1cf9
ZoroOneP <122513927+ZoroOneP@users.noreply.github.com>
2024-07-05 17:31:21 +0530

    Updates

---
+++ assets/FLConverter-BT8r5dLh.js
@@ -0,0 +1,1 @@
+const n=(r="")=>r===""?void 0:(r==null?void 0:r.charAt(0).toUpperCase())+(r==null?void 0:r.slice(1));export{n as F};
---
+++ assets/RuppessConverter-PMjyjO45.js
@@ -0,0 +1,1 @@
+const o=s=>{const e=s*83.06;return Math.floor(e)};export{o as R};
