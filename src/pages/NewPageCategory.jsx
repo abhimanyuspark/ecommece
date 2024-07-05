a27c6d96942780e325782ee2fc2854a553903677
ZoroOneP <122513927+ZoroOneP@users.noreply.github.com>
2024-07-05 15:11:08 +0530

    correct countewr function

--- src/pages/NewPageCategory.jsx
+++ src/pages/NewPageCategory.jsx
@@ -24,24 +24,26 @@ const NewPageCategory = () => {
       <div className="flex flex-col sm:gap-16 gap-4">
         {categoryProducts?.map((d, i) => (
           <div key={i} className="flex gap-4 flex-col">
-            <div className="relative">
-              <Link to={`/product/${d?.id}`}>
-                <div className="overflow-hidden">
-                  <img
-                    className="w-full h-72 blur"
-                    alt={d?.title}
-                    src={d?.thumbnail}
-                  />
-                </div>
-                <div className="absolute top-1/3 left-1/4">
-                  <h2 className="text-2xl">{d?.title}</h2>
+            <Link to={`/product/${d?.id}`} className="group/show">
+              <div
+                className="text-white bg-blue-500 h-52 flex items-center justify-center w-full bg-no-repeat"
+                style={{
+                  backgroundImage: `url(${d?.thumbnail})`,
+                }}
+              >
+                <div className="w-[600px] grid gap-2">
+                  <h2 className="text-2xl group-hover/show:underline">
+                    {d?.title}
+                  </h2>
                   <p>{d?.description}</p>
                 </div>
-              </Link>
-            </div>
+              </div>
+            </Link>
+
             <div className="flex gap-4 flex-wrap">
-              {d?.images.map((img, index) => (
+              {d?.images?.map((img, index) => (
                 <img
+                  loading="lazy"
                   className="sm:w-52 w-20 aspect-square"
                   key={index}
                   alt={img}
